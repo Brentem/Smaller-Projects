2643011acd65436ca766cd5dcb2b4b110240d74d
Brent van de Pol <32488142+Brentem@users.noreply.github.com>
2026-06-20 23:38:01 +0200

    Possible to convert int to string.

--- AssemblyCalc/README.md
+++ AssemblyCalc/README.md
@@ -7,6 +7,7 @@ Got Hello World example from this [repo](https://github.com/mschwartz/assembly-t
 * [FLAGS register](https://en.wikipedia.org/wiki/FLAGS_register)
 * [CMP Instruction Explanation](https://stackoverflow.com/a/61568237)
 * [x86_64 Linux Assembly #8 - Subroutine to Print Integers](https://www.youtube.com/watch?v=XuUD0WQ9kaE)
+  * https://pastebin.com/PN2jKVae
 * [x86_64 Linux Assembly #9 - Command Line Arguments](https://www.youtube.com/watch?v=xtFs1yBVinc)
 * [converting Integer to String process ("under the hood")](https://stackoverflow.com/a/67212831)
 * [Modern x64 Assembly](https://www.youtube.com/watch?v=rxsBghsrvpI&list=PLKK11Ligqitg9MOX3-0tFT1Rmh3uJp7kA)
--- AssemblyCalc/src/main.asm
+++ AssemblyCalc/src/main.asm
@@ -2,35 +2,82 @@
 bits 64
 
 section .data
-
 msg:    db      "Correct input is given!", 10
 .len:   equ     $ - msg
+
+; TODO: Make variables underneath local to _start.
+char_arr:	db 	16 dup (0)
+char_arr_count:	dq 	0
 
 section .text
         global _start
 
 _start:
-        pop     rax ; Get argc from stack
-        cmp     rax, 2 ; If argc != 2 go to exit
-        jne     .exit
+	; pop	rax	; Get argc from stack
+	; cmp	rax, 3	; If argc != 3 go to exit
+	; jne	.exit
 
-        pop     rbx ; Get arg1 from stack. In our casse this is ./build/main
-        pop     rbx ; Get arg2 from stack. In our case this is @
-        mov     rcx, [rbx] ; Write value which the memory address in rbx points to.
-                           ; See rbx as a pointer that is dereferenced.
+	; pop     rbx ; Get arg1 from stack. In our casse this is ./build/main
 
-        cmp     cl, 0x40 ; Check the lowest byte in the rcx register if it contains a @ character (0x40).
-        call	printTestOne
+	; TODO: Implement arg handling.
+        ; pop     rbx ; Get arg2 from stack.
+	; pop	rcx ; Get arg3 from stack.
 
-        .exit:
-        mov     rax, 60 ; exit
-        mov     rdi, 0
+	mov rbx, 719
+	add rbx, 200
+
+	mov rdi, rbx
+	mov rsi, char_arr
+	mov rcx, char_arr_count
+	call convert_str_to_int
+
+	; TODO: Add newline to char_arr.
+
+.print:
+        mov rax, 1 ; write
+        mov rdi, 1 ; stdout
+        mov rsi, char_arr
+        mov rdx, [char_arr_count]
         syscall
 
-printTestOne:
-        mov     rax, 1 ; write
-        mov     rdi, 1 ; stdout
-        mov     rsi, msg
-        mov     rdx, msg.len
+.exit:
+        mov rax, 60 ; exit
+        mov rdi, 0
         syscall
-        ret
+
+; rdi - Contains integer value that should be converted to string.
+; rsi - Contains pointer to byte buffer where string will be placed.
+; rcx - Contains pointer to variable where count of elements in buffer will be saved.
+convert_str_to_int:
+	push rbx ; Function uses callee-save registers that should be saved.
+	mov rax, rdi ; Put rdi input in rax register as dividend.
+	mov rbx, 10 ; Source operand for divisor.
+	mov r10, [rcx]
+
+LoopHead1:
+	xor rdx, rdx ; Zero out the rdx register, where remainder will be placed.
+
+	div rbx
+
+	add rdx, 0x30 ; Add value to remainder to make it the printable character equivalent in ASCII.
+	inc r10
+
+	xor r8, r8 ; Index
+LoopHead2:
+	mov r9, [rsi + r8] ; Copy original value of array with certain index.
+
+	mov [rsi + r8], dl
+
+	mov rdx, r9
+
+	inc r8
+	cmp r8, r10
+	jl LoopHead2
+
+	cmp rax, 0
+	jne LoopHead1
+
+	mov [rcx], r10
+
+	pop rbx ; Pop original value at start of function back into rbx.
+	ret
